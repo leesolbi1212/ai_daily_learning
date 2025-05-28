74be44bd05f6a72d59952f0cf24f2150f9e50c2b
이솔비 <dmddnjs77@gmail.com>
2025-05-28 16:23:01 +0900

    study

--- study/q_study.py
+++ study/q_study.py
@@ -5,8 +5,25 @@ queue_list = []
 queue_list.append(1)
 queue_list.append(2)
 queue_list.append(3)
-print("enqueue 후 리스트 큐:", queue_list)
+print("enqueue 후 리스트 큐:", queue_list) #enqueue 후 리스트 큐: [1, 2, 3]
 
 # dequeue: 큐에서 빼기
 queue_list.pop(0)  # 큐의 맨 앞에서 빼기 (느림 O(n))
-print("dequeue 후 리스트 큐:", queue_list)
+print("dequeue 후 리스트 큐:", queue_list) #dequeue 후 리스트 큐: [2, 3]
+
+# collections.deque 사용
+
+from collections import deque
+
+# deque 큐 생성
+queue_deque = deque()
+
+# enqueue: O(1)
+queue_deque.append(1)
+queue_deque.append(2)
+queue_deque.append(3)
+print("enqueue 후 deque 큐:", queue_deque)
+
+# dequeue: O(1)
+queue_deque.popleft()
+print("dequeue 후 deque 큐:", queue_deque)
