781f3510971c87a1f51ee46c2db77e1d6d03271d
이솔비 <dmddnjs77@gmail.com>
2025-06-20 16:23:01 +0900

    study

--- crawling/2_webtoon.py
+++ crawling/2_webtoon.py
@@ -1,11 +1,11 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By #파싱하는 종류들을 상수화
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()
 driver.get('https://comic.naver.com/webtoon/detail?titleId=650305&no=417&week=sat')
-soup = BeautifulSoup(driver.page_source,"html.parser")
+# soup = BeautifulSoup(driver.page_source,"html.parser")
 time.sleep(2)
 
 print('**베스트 댓글**')
@@ -17,6 +17,24 @@ for li in best_comment_elements:
         comment_p = li.find_element(By.XPATH, './div/div[2]/div/p') # xpath 이후의 경로를 작성해줘야 함 
         comment_text = comment_p.text.strip()
         print(comment_text)
-        print('-' * 30)
+        print('-' * 40)
+    except Exception as e:
+        print(e)
+
+# 전체 댓글 클릭 
+driver.find_element(By.XPATH, '//*[@id="wcc_root"]/section/div[4]/button[2]').click()
+time.sleep(2)
+
+# soup = BeautifulSoup(driver.page_source,"html.parser")
+print('**전체 댓글**')
+xpath = '/html/body/div[1]/div[5]/div/div/div[4]/div[1]/div[3]/div/section/ul/li'
+total_comment_elements = driver.find_elements(By.XPATH, xpath)
+
+for li in total_comment_elements:
+    try:
+        comment_p = li.find_element(By.XPATH, './div/div[2]/div/p') # xpath 이후의 경로를 작성해줘야 함 
+        comment_text = comment_p.text.strip()
+        print(comment_text)
+        print('-' * 40)
     except Exception as e:
         print(e)
